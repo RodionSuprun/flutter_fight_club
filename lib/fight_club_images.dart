d2b552c32b0fc1be2c5e343e50234156fd32e343
rodion.suprun <rodion.suprun@homecredit.kz>
2023-04-28 16:25:24 +0600

    Rename images

--- lib/fight_club_images.dart
+++ lib/fight_club_images.dart
@@ -1,6 +1,6 @@
 class FightClubImages {
 
   FightClubImages._();
-  static const youAvatar = "assets/images/youAvatar.png";
-  static const enemyAvatar = "assets/images/enemyAvatar.png";
+  static const youAvatar = "assets/images/you-avatar.png";
+  static const enemyAvatar = "assets/images/enemy-avatar.png";
 }
