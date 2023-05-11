df91369ac27a7ecad992a1443c1d77e1a9cd2438
rodion.suprun <rodion.suprun@homecredit.kz>
2023-05-11 13:42:44 +0600

    Changes after lesson 5

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -31,56 +31,40 @@ class _StatisticsPage extends StatelessWidget {
                 ),
               ),
               const Expanded(child: SizedBox()),
-              Column(
-                children: [
-                  FutureBuilder<int?>(
-                    future: SharedPreferences.getInstance()
-                        .then((sharedPreferences) {
-                      return sharedPreferences.getInt("stats_won");
-                    }),
-                    builder: (context, snapshot) {
-                      return Text(
-                        "Won: ${snapshot.data ?? 0}",
+              FutureBuilder<SharedPreferences>(
+                future: SharedPreferences.getInstance(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData || snapshot.data == null) {
+                    return const SizedBox();
+                  }
+                  return Column(
+                    children: [
+                      Text(
+                        "Won: ${snapshot.data!.getInt("stats_won") ?? 0}",
                         style: const TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w400,
                             color: FightClubColors.darkGreyText),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 6),
-                  FutureBuilder<int?>(
-                    future: SharedPreferences.getInstance()
-                        .then((sharedPreferences) {
-                      return sharedPreferences.getInt("stats_draw");
-                    }),
-                    builder: (context, snapshot) {
-                      return Text(
-                        "Draw: ${snapshot.data ?? 0}",
+                      ),
+                      const SizedBox(height: 6),
+                      Text(
+                        "Draw: ${snapshot.data!.getInt("stats_draw") ?? 0}",
                         style: const TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w400,
                             color: FightClubColors.darkGreyText),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 6),
-                  FutureBuilder<int?>(
-                    future: SharedPreferences.getInstance()
-                        .then((sharedPreferences) {
-                      return sharedPreferences.getInt("stats_lost");
-                    }),
-                    builder: (context, snapshot) {
-                      return Text(
-                        "Lost: ${snapshot.data ?? 0}",
+                      ),
+                      const SizedBox(height: 6),
+                      Text(
+                        "Lost: ${snapshot.data!.getInt("stats_lost") ?? 0}",
                         style: const TextStyle(
                             fontSize: 16,
                             fontWeight: FontWeight.w400,
                             color: FightClubColors.darkGreyText),
-                      );
-                    },
-                  ),
-                ],
+                      )
+                    ],
+                  );
+                },
               ),
               const Expanded(child: SizedBox()),
               Padding(
